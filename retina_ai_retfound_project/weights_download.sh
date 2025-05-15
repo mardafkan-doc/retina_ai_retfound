0f4b9225d89d044efb9407fb718ff95f6216937e
mardafkan <haveafoodvk@gmail.com>
2025-05-15 18:56:20 +0500

    Фикс команды gdown -добавлен пробел перед -O

--- retina_ai_retfound_project/weights_download.sh
+++ retina_ai_retfound_project/weights_download.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gdown --id 160tgvEj0vtMwbyN8joOiUEAGumhB4Hl1 -O RETFound_oct_weights.pth
+gdown "https://drive.google.com/uc?export=download&id=160tgvEj0vtMwbyN8joOiUEAGumhB4H11" -O RETFound_oct_weights.pth
